502cd467199f3cdc8daa79c0be4dae38e1c663af
Luiz Eduardo <eduardogois04@gmail.com>
2024-01-07 10:32:32 -0300

    DESAFIO CONCLUÍDO!

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Grato por todo aprendizado nesse bootcamp! Espero ser útil para o mercado e para a sociedade em breve.");
